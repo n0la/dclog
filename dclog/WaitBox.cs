db28ad00d9e81583233f29f1568c2bd732e70735
Florian Stinglmayr <fstinglmayr@gmail.com>
2012-02-12 20:09:32 +0100

    Added GPL v3 header to all source files.
     With real name, now it's all too late, ban me Turbine!

--- dclog/WaitBox.cs
+++ dclog/WaitBox.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+ * DCLog ~ A DDO Combat Logger
+ * Copyright (C) 2012 Florian Stinglmayr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,28 +27,28 @@ using System.Windows.Forms;
 
 namespace dclog
 {
-    public partial class WaitBox : Form
+  public partial class WaitBox : Form
+  {
+    public WaitBox()
     {
-        public WaitBox()
-        {
-            InitializeComponent();
-        }
-
-        public string InfoText
-        {
-            get { return text.Text; }
-            set { text.Text = value; }
-        }
-
-        private void WaitBox_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void cancel_Click(object sender, EventArgs e)
-        {
-            this.DialogResult = DialogResult.Cancel;
-            this.Close();
-        }
+      InitializeComponent();
     }
+
+    public string InfoText
+    {
+      get { return text.Text; }
+      set { text.Text = value; }
+    }
+
+    private void WaitBox_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private void cancel_Click(object sender, EventArgs e)
+    {
+      this.DialogResult = DialogResult.Cancel;
+      this.Close();
+    }
+  }
 }
--- ddohook/Properties/AssemblyInfo.cs
+++ ddohook/Properties/AssemblyInfo.cs
@@ -1,4 +1,22 @@
-﻿using System.Reflection;
+﻿/*
+ * DCLog ~ A DDO Combat Logger
+ * Copyright (C) 2012 Florian Stinglmayr
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -8,9 +26,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ddohook")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ddohook")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
