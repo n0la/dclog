3c4039a81bd9bc9b7785a1fb67ca5d49128d09d0
Florian Stinglmayr <fstinglmayr@gmail.com>
2014-10-10 14:05:41 +0200

    Updated version string in AboutDialog.

--- dclog/AboutDialog.Designer.cs
+++ dclog/AboutDialog.Designer.cs
@@ -31,15 +31,15 @@
       System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutDialog));
       this.tabControl1 = new System.Windows.Forms.TabControl();
       this.About = new System.Windows.Forms.TabPage();
-      this.tabPage2 = new System.Windows.Forms.TabPage();
-      this.label1 = new System.Windows.Forms.Label();
-      this.label2 = new System.Windows.Forms.Label();
-      this.label3 = new System.Windows.Forms.Label();
-      this.label4 = new System.Windows.Forms.Label();
-      this.label5 = new System.Windows.Forms.Label();
-      this.projectsite = new System.Windows.Forms.LinkLabel();
-      this.gpl = new System.Windows.Forms.TextBox();
       this.label6 = new System.Windows.Forms.Label();
+      this.projectsite = new System.Windows.Forms.LinkLabel();
+      this.label5 = new System.Windows.Forms.Label();
+      this.label4 = new System.Windows.Forms.Label();
+      this.label3 = new System.Windows.Forms.Label();
+      this.label2 = new System.Windows.Forms.Label();
+      this.label1 = new System.Windows.Forms.Label();
+      this.tabPage2 = new System.Windows.Forms.TabPage();
+      this.gpl = new System.Windows.Forms.TextBox();
       this.tabControl1.SuspendLayout();
       this.About.SuspendLayout();
       this.tabPage2.SuspendLayout();
@@ -73,67 +73,15 @@
       this.About.Text = "About";
       this.About.UseVisualStyleBackColor = true;
       // 
-      // tabPage2
+      // label6
       // 
-      this.tabPage2.Controls.Add(this.gpl);
-      this.tabPage2.Location = new System.Drawing.Point(4, 22);
-      this.tabPage2.Name = "tabPage2";
-      this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-      this.tabPage2.Size = new System.Drawing.Size(549, 315);
-      this.tabPage2.TabIndex = 1;
-      this.tabPage2.Text = "License";
-      this.tabPage2.UseVisualStyleBackColor = true;
-      // 
-      // label1
-      // 
-      this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label1.Location = new System.Drawing.Point(14, 93);
-      this.label1.Name = "label1";
-      this.label1.Size = new System.Drawing.Size(527, 37);
-      this.label1.TabIndex = 0;
-      this.label1.Text = "An application to view and analyze combat log created by the MMORPG Dungeons and " +
-          "Dragons: Online.";
-      // 
-      // label2
-      // 
-      this.label2.AutoSize = true;
-      this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label2.ForeColor = System.Drawing.Color.Red;
-      this.label2.Location = new System.Drawing.Point(8, 143);
-      this.label2.Name = "label2";
-      this.label2.Size = new System.Drawing.Size(66, 16);
-      this.label2.TabIndex = 1;
-      this.label2.Text = "Warning!";
-      // 
-      // label3
-      // 
-      this.label3.AutoSize = true;
-      this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label3.ForeColor = System.Drawing.Color.Black;
-      this.label3.Location = new System.Drawing.Point(8, 54);
-      this.label3.Name = "label3";
-      this.label3.Size = new System.Drawing.Size(164, 16);
-      this.label3.TabIndex = 2;
-      this.label3.Text = "DCLog - Version 0.1 Beta";
-      // 
-      // label4
-      // 
-      this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label4.Location = new System.Drawing.Point(11, 163);
-      this.label4.Name = "label4";
-      this.label4.Size = new System.Drawing.Size(530, 91);
-      this.label4.TabIndex = 3;
-      this.label4.Text = resources.GetString("label4.Text");
-      // 
-      // label5
-      // 
-      this.label5.AutoSize = true;
-      this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label5.Location = new System.Drawing.Point(136, 19);
-      this.label5.Name = "label5";
-      this.label5.Size = new System.Drawing.Size(276, 16);
-      this.label5.TabIndex = 5;
-      this.label5.Text = "\"May the goddess keep us from single vision.\"";
+      this.label6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label6.Location = new System.Drawing.Point(14, 74);
+      this.label6.Name = "label6";
+      this.label6.Size = new System.Drawing.Size(527, 22);
+      this.label6.TabIndex = 7;
+      this.label6.Text = "Copyright 2012 by Florian Stinglmayr, et.al. See COPYRIGHTS for further informati" +
+    "on.";
       // 
       // projectsite
       // 
@@ -147,6 +95,68 @@
       this.projectsite.Text = "https://github.com/n0la/dclog";
       this.projectsite.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
       // 
+      // label5
+      // 
+      this.label5.AutoSize = true;
+      this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label5.Location = new System.Drawing.Point(136, 19);
+      this.label5.Name = "label5";
+      this.label5.Size = new System.Drawing.Size(276, 16);
+      this.label5.TabIndex = 5;
+      this.label5.Text = "\"May the goddess keep us from single vision.\"";
+      // 
+      // label4
+      // 
+      this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label4.Location = new System.Drawing.Point(11, 163);
+      this.label4.Name = "label4";
+      this.label4.Size = new System.Drawing.Size(530, 91);
+      this.label4.TabIndex = 3;
+      this.label4.Text = resources.GetString("label4.Text");
+      // 
+      // label3
+      // 
+      this.label3.AutoSize = true;
+      this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label3.ForeColor = System.Drawing.Color.Black;
+      this.label3.Location = new System.Drawing.Point(8, 54);
+      this.label3.Name = "label3";
+      this.label3.Size = new System.Drawing.Size(164, 16);
+      this.label3.TabIndex = 2;
+      this.label3.Text = "DCLog - Version 0.3 Beta";
+      // 
+      // label2
+      // 
+      this.label2.AutoSize = true;
+      this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label2.ForeColor = System.Drawing.Color.Red;
+      this.label2.Location = new System.Drawing.Point(8, 143);
+      this.label2.Name = "label2";
+      this.label2.Size = new System.Drawing.Size(66, 16);
+      this.label2.TabIndex = 1;
+      this.label2.Text = "Warning!";
+      // 
+      // label1
+      // 
+      this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.label1.Location = new System.Drawing.Point(14, 93);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(527, 37);
+      this.label1.TabIndex = 0;
+      this.label1.Text = "An application to view and analyze combat log created by the MMORPG Dungeons and " +
+    "Dragons: Online.";
+      // 
+      // tabPage2
+      // 
+      this.tabPage2.Controls.Add(this.gpl);
+      this.tabPage2.Location = new System.Drawing.Point(4, 22);
+      this.tabPage2.Name = "tabPage2";
+      this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+      this.tabPage2.Size = new System.Drawing.Size(549, 315);
+      this.tabPage2.TabIndex = 1;
+      this.tabPage2.Text = "License";
+      this.tabPage2.UseVisualStyleBackColor = true;
+      // 
       // gpl
       // 
       this.gpl.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -159,16 +169,6 @@
       this.gpl.Size = new System.Drawing.Size(543, 309);
       this.gpl.TabIndex = 0;
       this.gpl.Text = resources.GetString("gpl.Text");
-      // 
-      // label6
-      // 
-      this.label6.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-      this.label6.Location = new System.Drawing.Point(14, 74);
-      this.label6.Name = "label6";
-      this.label6.Size = new System.Drawing.Size(527, 22);
-      this.label6.TabIndex = 7;
-      this.label6.Text = "Copyright 2012 by Florian Stinglmayr, et.al. See COPYRIGHTS for further informati" +
-          "on.";
       // 
       // AboutDialog
       // 
